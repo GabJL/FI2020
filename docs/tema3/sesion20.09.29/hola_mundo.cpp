156ab0393771b7d623149d0484ab8bf82e135e80
Gabriel Luque <GabJL@users.noreply.github.com>
2020-09-29 07:14:10 +0200

    Create hola_mundo.cpp

---
+++ docs/tema3/sesion20.09.29/hola_mundo.cpp
@@ -0,0 +1,12 @@
+/* Programa: hola mundo
+   Objetivo: Escribir una línea por pantalla */
+#include <iostream>
+
+using namespace std;
+
+// Función principal
+int main()
+{
+	cout << "Hola mundo!" << endl;
+	return 0;
+}
