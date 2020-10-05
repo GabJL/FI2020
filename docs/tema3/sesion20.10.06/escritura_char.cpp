52ca7b71700481c20e0f36d7565e5956038a4491
Gabriel Luque <gabrielluque@pdi-134-213.wifi.uma.es>
2020-10-05 12:16:48 +0200

    Clase 4. Paso 4

--- docs/tema3/sesion20.10.06/escritura_char.cpp
+++ docs/tema3/sesion20.10.06/escritura_char.cpp
@@ -8,28 +8,28 @@ int main(){
     char letra3 = 50; // código ASCII de ‘2’: 50
 
     cout << "Caso 1: ";
-    cout << letra2;						
+    cout << letra2;
     
     cout << endl << "Caso 2: ";
     letra1 = letra2 + 3;
-	cout << letra1;						
+	cout << letra1;
     
-    cout << endl << "Caso 2: ";
-    cout << letra2 + 3;					
+    cout << endl << "Caso 3: ";
+    cout << letra2 + 3;
     
-    cout << endl << "Caso 2: ";
-    cout << char(letra2 + 3);					
+    cout << endl << "Caso 4: ";
+    cout << char(letra2 + 3);
     
-    cout << endl << "Caso 2: ";
-    cout << letra3;						
+    cout << endl << "Caso 5: ";
+    cout << letra3;
     
-    cout << endl << "Caso 2: ";
-    cout << int(letra3);					
+    cout << endl << "Caso 6: ";
+    cout << int(letra3);
     
-    cout << endl << "Caso 2: ";
-    cout << letra3 - '0';					
+    cout << endl << "Caso 7: ";
+    cout << letra3 - '0';
     
-    cout << endl << "Caso 2: ";
+    cout << endl << "Caso 8: ";
     cout << char(letra3 - '0');
     cout << endl;
 
