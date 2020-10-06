983444746212b888cd82f6bbe1ba3b8d84f520ff
Gabriel Luque <gluque@uma.es>
2020-10-06 17:21:55 +0200

    Código inicial para el ejercicio 1 de la p1

---
+++ prácticas/p1/ejercicio1-inicial.cpp
@@ -0,0 +1,35 @@
+/****************************************************************************** 
+* Programa para el cálculo del diámetro, longitud y  área de una 
+* circunferencia. Se pide al usuario que se introduzca el radio por teclado 
+* y se muestran los resultados por pantalla. 
+* 
+* Programador: Patricio Estrella 
+* Fecha: 09/10/2020 
+*******************************************************************************/ 
+
+#include iostream 
+
+const int PI 3,14 
+
+void main() 
+{ 
+  double radio, longitud, area; // Declaración de variables 
+
+  cout << "Teclee la medida del radio de la circunferencia en cm" << endl; 
+  cin >> radio 
+
+  // Cálculos de magnitudes 
+  diametro = 2.0 * radio; 
+  PI * diámetro = longitud; 
+  area = PI * radio * radio; 
+
+  // Salida de resultados 
+  del programa // 
+
+  cout << "DATOS DEL CIRCULO:" << endl; 
+  cout << "Radio introducido por el usuario: " << radio << " cm" << endl; 
+  cout << "Diametro de la circunferencia: " << diametro << " cm" << endl; 
+  cout << "Longitud de la circunferencia: " << longitud << " cm" << endl; 
+  cout << "Area de la circunferencia: " << area << " cm2" << endl; 
+
+} 
