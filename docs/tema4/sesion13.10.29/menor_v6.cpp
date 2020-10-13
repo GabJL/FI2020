8082d52755dfd206d79370a271f9ca0056193d40
Gabriel Luque <GabJL@users.noreply.github.com>
2020-10-13 10:29:49 +0200

    Create menor_v6.cpp

---
+++ docs/tema4/sesion13.10.29/menor_v6.cpp
@@ -0,0 +1,20 @@
+// Tema 4: Sentencias de selección (alternativas)
+// Programa que calcule el menor de dos números (version 6)
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int a, b, menor;
+	
+	cout << "Dime dos numeros: ";
+	cin >> a >> b;
+
+	menor = b;
+	if(a < menor){
+		menor = a;
+	}
+	cout << "El menor es: " << menor << endl;
+
+	return 0;
+}
