2fd6d7fb562bacbb8faa20c08854b63b8e7c925a
Gabriel Luque <GabJL@users.noreply.github.com>
2020-10-27 21:15:53 +0100

    Create que_hace_for.cpp

---
+++ docs/tema4/sesion27.10.20/que_hace_for.cpp
@@ -0,0 +1,11 @@
+// Sentencias de repetición (for): ¿Qué hace el código?
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    for (int i = 0; i < 11; i++){ 
+        cout << i << " -> " << i*i << endl;
+    }
+    return 0;
+}
