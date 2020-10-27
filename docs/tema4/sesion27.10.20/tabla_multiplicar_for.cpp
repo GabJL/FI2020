cc3318e342c51c63a51593f744e242dc517468e0
Gabriel Luque <GabJL@users.noreply.github.com>
2020-10-27 21:15:08 +0100

    Create tabla_multiplicar_for.cpp

---
+++ docs/tema4/sesion27.10.20/tabla_multiplicar_for.cpp
@@ -0,0 +1,11 @@
+// Sentencias de repetición (for): Tabla de multiplicar
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	for(int i = 1; i <= 10; i++){
+		cout << "7 x " << i << " = " << i*7 << endl;
+	}
+	return 0;
+}
