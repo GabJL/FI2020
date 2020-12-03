45eafa6b1d033cb4d12157e9b98877869db6fcd3
Gabriel Luque <GabJL@users.noreply.github.com>
2020-12-03 18:11:28 +0100

    Update string.cpp

--- docs/tema6/sesion3.12.20/string.cpp
+++ docs/tema6/sesion3.12.20/string.cpp
@@ -1,36 +1,59 @@
 #include <iostream>
 #include <string>
-#include <cctype>
+#include <cctype> // Para usar el toupper
 
 using namespace std;
 
-void pasarMayuscula(string &s){
-    for(int i = 0; i < s.size(); i++){
-        s[i] = toupper(s[i]);
+// Funciones
+void pasar_a_mayuscula(string &palabra){
+    int tam = palabra.size();
+    for(int i = 0; i < tam;i++){
+        palabra[i] = toupper(palabra[i]);
     }
 }
 
+// acortar_acronimo:
+// Si tiene <= 3 letras -> No hace nada
+// Sino -> Devuelve una nueva palabra que contenga 3 letras: las dos primeras y la última del acronimo
+string acortar_acronimo(string acronimo){
+    string nuevo_acronimo = acronimo;
+    if(acronimo.size() > 3){
+        nuevo_acronimo = "" + acronimo[0] + acronimo[1] + acronimo[acronimo.size()-1];
+    }
+    return nuevo_acronimo;
+}
+
 int main(){
-    string palabra, mayor, mas_corta, acronimo;
+    string palabra, mayor = "", mas_corta, acronimo = "";
     int contador = 0;
-
-    cout << "Diga palabras acabadas en FIN: ";
-
+    
+    cout << "Diga unas palabras acabadas en FIN: ";
     cin >> palabra;
-    mayor = mas_corta = palabra;
+    mayor = palabra;
+    mas_corta = palabra;
     while(palabra != "FIN"){
+        // Hacer lo que se pida con palabras
         contador++;
-        if(palabra > mayor) mayor = palabra;
-        if(palabra.size() < mas_corta.size()) mas_corta = palabra;
-        acronimo += palabra[0];
+        if(palabra > mayor){
+            mayor = palabra;
+        }
+
+        if(palabra.size() < mas_corta.size()){
+            mas_corta = palabra;
+        }
+
+        acronimo = acronimo + palabra[0];
+
         cin >> palabra;
     }
 
-    cout << "Se han leido " << contador << " palabras validas" << endl;
-    cout << "La palabra mayor es: " << mayor << endl;
-    cout << "La palabra mas corta es: " << mas_corta << endl;
-    pasarMayuscula(acronimo);
-    cout << "El acronimo es " << acronimo << endl;
+//    char letra = 'a';
+//    char letra_may = toupper(letra);
+    pasar_a_mayuscula(acronimo);
+    cout << "El acronimo es: " << acortar_acronimo(acronimo) << endl;
+    cout << "La cantidad de palabras han sido " << contador << endl;
+    cout << "La mayor palabra fue " << mayor << endl;
+    cout << "La palabra mas corta es " << mas_corta << " con tamanio " << mas_corta.size() << endl;
 
     return 0;
 }
