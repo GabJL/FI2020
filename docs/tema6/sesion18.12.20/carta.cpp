171b3e87d6f3711877b93ef4b3bde62c789e4d1a
Gabriel Luque <GabJL@users.noreply.github.com>
2020-12-18 16:35:53 +0100

    Update carta.cpp

--- docs/tema6/sesion18.12.20/carta.cpp
+++ docs/tema6/sesion18.12.20/carta.cpp
@@ -5,65 +5,92 @@
 
 using namespace std;
 
-typedef struct {
-	int valor;
-	string figura;
+// Definir TCarta (valor y figura)
+typedef struct{
+    int valor;
+    string figura;
 } TCarta;
 
-void leerCarta(TCarta &c){
-	do{
-		cout << "Dime el valor (1-10) de la carta: ";
-		cin >> c.valor;
-	}while(c.valor < 1 or c.valor > 10);
-
-	do{
-		cout << "Dime la figura (picas, corazones, treboles o diamantes) de la carta: ";
-		cin >> c.figura;
-	} while(c.figura != "picas" and c.figura != "corazones" and c.figura != "treboles" and c.figura != "diamantes");
+void leer_carta(TCarta &c){
+    cout << "Valor: ";
+    cin >> c.valor;
+    cout << "Figura: ";
+    cin >> c.figura;
 }
 
-void mostrarCarta(const TCarta &c){ // Parámetro de entrada: referencia constante
-	cout << c.valor << " de " << c.figura << endl;
+void mostrar_carta(const TCarta &c){
+    cout << c.valor << " de " << c.figura << endl;
 }
 
-TCarta generarCarta(){
-	TCarta c;
-	c.valor = rand()%10 + 1;
-	switch(rand()%4){
-		case 0: c.figura = "picas"; break;
-		case 1: c.figura = "corazones"; break;
-		case 2: c.figura = "treboles"; break;
-		case 3: c.figura = "diamantes"; break;
-	}
-	return c;
+TCarta generar_aleatoria(){
+    TCarta c;
+
+    srand(time(0));
+
+    c.valor = (rand()%10) + 1;
+
+    switch(rand()%4){
+        case 0: c.figura = "corazones"; break;
+        case 1: c.figura = "picas"; break;
+        case 2: c.figura = "treboles"; break;
+        case 3: c.figura = "diamantes"; break;
+    }
+
+
+    return c;
 }
 
-bool iguales(const TCarta &c1, const TCarta &c2){
-	return c1.valor == c2.valor and c1.figura == c2.figura;
+bool PrimeraEsMayor(const TCarta &c1, const TCarta &c2){
+    bool res=false;
+
+    if(c1.valor > c2.valor){
+        res=true;
+    }else if(c1.valor==c2.valor){
+        if(c1.figura>c2.figura){
+            res=true;
+        }
+    }
+    return res;
+    //return (c1.valor > c2.valor || (c1.valor == c2.valor && c1.figura > c2.figura));
 }
 
-bool menor(const TCarta &c1, const TCarta &c2){
-	return (c1.valor < c2.valor) or ((c1.valor == c2.valor) and (c1.figura < c2.figura));
+bool comprobar_si_iguales(const TCarta &c1, const TCarta &c2)
+{
+  bool res=true;
+
+  if(c1.valor!=c2.valor || c1.figura!=c2.figura)
+  {
+    res=false;
+  }
+  return res;
 }
 
 int main(){
-	TCarta cJug, cCPU;
+    /*TCarta carta1 = {9, "corazones"}, carta2;
 
-	srand(time(0)); // Inicializamos los números aleatorios
+    mostrar_carta(carta1);
+    leer_carta(carta1);
+    mostrar_carta(carta1);
+    carta2 = generar_aleatoria();
+    mostrar_carta(carta2);*/
 
-	leerCarta(cJug);
-	do{
-		cCPU = generarCarta();
-	}while(iguales(cJug, cCPU));
+    TCarta carta1, carta2;
+    leer_carta(carta1);
 
-	cout << "El ordenador eligio: ";
-	mostrarCarta(cCPU);
+    do{
+        carta2 = generar_aleatoria();
+    }while(comprobar_si_iguales(carta1, carta2));
 
-	if(menor(cCPU, cJug)){
-		cout << "Ha ganado el Jugador" << endl;
-	} else {
-		cout << "Ha ganado la CPU" << endl;
-	}
+    cout << "Carta del usuario: ";
+    mostrar_carta(carta1);
+    cout << "Carta del ordenador: ";
+    mostrar_carta(carta2);
 
-	return 0;
-} 
+    if(PrimeraEsMayor(carta1, carta2)){
+        cout << "Ganas tu!" << endl;
+    } else {
+        cout << "Gana el ordenador" << endl;
+    }
+
+    return 0;
+}
