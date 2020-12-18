4950eb3368fc4b4962bf83e794123d7c9f96de4a
Gabriel Luque <GabJL@users.noreply.github.com>
2020-12-18 16:36:22 +0100

    Update libros.cpp

--- docs/tema6/sesion18.12.20/libros.cpp
+++ docs/tema6/sesion18.12.20/libros.cpp
@@ -3,35 +3,44 @@
 using namespace std;
 
 // tipos
-typedef struct {
-	string titulo;
-	double precio;
-	int paginas;
-	string editorial;
+
+typedef struct{
+    string nombre;
+    double precio;
+    int paginas;
+    string editorial;
 } TLibro;
 
-
 int main(){
-	TLibro libro1, libro2 = {"El Marciano", 20.5, 407, "Nova"};
+    TLibro libro1, libro2 = {"El Marciano", 20.5, 407, "Nova"};
 
-	cout << "Dime los datos de un libro: " << endl;
-	cout << "Titulo: "; getline(cin, libro1.titulo); // El título pueden ser varias palabras
-	cout << "Precio: "; cin >> libro1.precio;
-	cout << "Paginas: "; cin >> libro1.paginas;
-	cout << "Editorial: "; cin >> libro1.editorial; // Supondremos que la editorial es una única palabra
+    // MAL: cin >> libro1;
+    cout << "Nombre del libro: ";
+    // Solo lee una palabra: cin >> libro1.nombre;
+    getline(cin, libro1.nombre);
+    cout << "Precio: ";
+    cin >> libro1.precio;
+    cout << "Paginas: ";
+    cin >> libro1.paginas;
+    cout << "Editorial: ";
+    cin >> libro1.editorial;
 
-	if(libro1.paginas > libro2.paginas){
-		cout << "El libro " << libro1.titulo << " tiene mas paginas (" << libro1.paginas << ")" << endl;
-	} else {
-		cout << "El libro " << libro2.titulo << " tiene mas paginas (" << libro2.paginas << ")" << endl;
-	}
+    if(libro1.paginas > libro2.paginas){
+        cout << libro1.nombre << " con " << libro1.paginas << " paginas" << endl;
+    } else {
+        cout << libro2.nombre << " con " << libro2.paginas << " paginas" << endl;
+    }
 
-	if(libro1.precio > libro2.precio){
-		libro1.precio *= 0.95;
-		cout << "El libro " << libro1.titulo << " cuesta ahora " << libro1.precio << " euros" << endl;		
-	} else {
-		libro2.precio *= 0.95;
-		cout << "El libro " << libro2.titulo << " cuesta ahora " << libro2.precio << " euros" << endl;		
-	}
-	return 0;
+    TLibro mas_caro;
+    if(libro1.precio > libro2.precio){
+        libro1.precio = libro1.precio*0.95;
+        mas_caro = libro1;
+    } else {
+        libro2.precio = libro2.precio*0.95;
+        mas_caro = libro2;
+    }
+
+    cout << "El libro " << mas_caro.nombre << " ahora cuesta " << mas_caro.precio << " euros" << endl;
+
+    return 0;
 }
